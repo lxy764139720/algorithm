950baba0d32a48b49899e1d09db9aae0d6599967
lxy764139720 <764139720@qq.com>
2019-09-24 09:33:37 +0800

    141、142

--- leetcode/142.环形链表-ii.c
+++ leetcode/142.环形链表-ii.c
@@ -1,28 +1,30 @@
 /*
- * @lc app=leetcode.cn id=141 lang=c
+ * @lc app=leetcode.cn id=142 lang=c
  *
- * [141] 环形链表
+ * [142] 环形链表 II
  *
- * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/description/
  *
  * algorithms
- * Easy (42.98%)
- * Likes:    382
+ * Medium (43.94%)
+ * Likes:    238
  * Dislikes: 0
- * Total Accepted:    67.8K
- * Total Submissions: 157.2K
+ * Total Accepted:    30K
+ * Total Submissions: 68K
  * Testcase Example:  '[3,2,0,-4]\n1'
  *
- * 给定一个链表，判断链表中是否有环。
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
  * 
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 说明：不允许修改给定的链表。
  * 
  * 
  * 
  * 示例 1：
  * 
  * 输入：head = [3,2,0,-4], pos = 1
- * 输出：true
+ * 输出：tail connects to node index 1
  * 解释：链表中有一个环，其尾部连接到第二个节点。
  * 
  * 
@@ -31,7 +33,7 @@
  * 示例 2：
  * 
  * 输入：head = [1,2], pos = 0
- * 输出：true
+ * 输出：tail connects to node index 0
  * 解释：链表中有一个环，其尾部连接到第一个节点。
  * 
  * 
@@ -40,7 +42,7 @@
  * 示例 3：
  * 
  * 输入：head = [1], pos = -1
- * 输出：false
+ * 输出：no cycle
  * 解释：链表中没有环。
  * 
  * 
@@ -49,8 +51,7 @@
  * 
  * 
  * 进阶：
- * 
- * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 你是否可以不用额外空间解决此题？
  * 
  */
 /**
@@ -60,30 +61,27 @@
  *     struct ListNode *next;
  * };
  */
-bool hasCycle(struct ListNode *head)
+struct ListNode *detectCycle(struct ListNode *head)
 {
-    if (!head)
+    if (!head || !head->next)
     {
         return NULL;
     }
-    struct ListNode *p1 = head;
-    struct ListNode *p2 = head;
-    while (p2)
+    struct ListNode *low = head;
+    struct ListNode *fast = head;
+    do
     {
-        p1 = p1->next;
-        p2 = p2->next;
-        if (p2)
-        {
-            p2 = p2->next;
-        }
-        else
+        if (!fast || !fast->next)
         {
             return false;
         }
-        if (p1 == p2)
-        {
-            return true;
-        }
+        fast = fast->next->next;
+        low = low->next;
+    } while (low != fast);
+    while (head != low)
+    {
+        head = head->next;
+        low = low->next;
     }
-    return false;
+    return low;
 }
